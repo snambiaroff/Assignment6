591f1ccc09b88c21802bd97465e6eed0a5418200
snambiaroff <144677752+snambiaroff@users.noreply.github.com>
2023-10-05 23:58:52 +0530

    Create pgm16.py

---
+++ pgm16.py
@@ -0,0 +1,10 @@
+list1 = []
+
+# Take inputs from the user to make the list
+while True:
+  # Prompt the user to enter a number
+  number = input("Enter a number (press q to quit): ")
+
+  # If the user presses q, break out of the loop
+  if number == "q":
+    break
