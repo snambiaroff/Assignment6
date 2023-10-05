e8fd0320ff658beddd8befa5f1f079798739fe0e
snambiaroff <144677752+snambiaroff@users.noreply.github.com>
2023-10-05 23:59:45 +0530

    Create pgm18.py

---
+++ pgm18.py
@@ -0,0 +1,12 @@
+# Create two empty lists
+list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+list_2 = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+
+# Create new lists for each divisor
+divisible_by_4 = []
+divisible_by_6 = []
+divisible_by_8 = []
+divisible_by_10 = []
+divisible_by_3 = []
+divisible_by_5 = []
+divisible_by_7 = []
