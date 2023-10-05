4e75590cc8e388509d2e93b17afff31f839f1cfe
snambiaroff <144677752+snambiaroff@users.noreply.github.com>
2023-10-06 00:00:09 +0530

    Create pgm19.py

---
+++ pgm19.py
@@ -0,0 +1,12 @@
+def separate_ints_strings_floats(list1):
+  """Separates ints, strings, and floats from a list and stores them in separate lists.
+
+  Args:
+    list1: A list containing ints, strings, and floats.
+
+  Returns:
+    Three lists, one containing ints, one containing strings, and one containing floats.
+  """
+
+  int_list = []
+  string_list = []
