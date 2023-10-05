443cbf2e6c693cc55216cdbf3b3e1369a69bd9ca
snambiaroff <144677752+snambiaroff@users.noreply.github.com>
2023-10-05 23:57:48 +0530

    Create pgm14.py

---
+++ pgm14.py
@@ -0,0 +1,11 @@
+def print_multiplication_table(number):
+ 
+  for i in range(1, 11):
+    print(f"{number} x {i} = {number * i}")
+
+
+print("Multiplication table of 24:")
+print_multiplication_table(24)
+
+print("Multiplication table of 50:")
+print_multiplication_table(50)
